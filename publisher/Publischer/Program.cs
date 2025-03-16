9b7557c4f1ba16f73d3067570de35c237473b148
Sergio Manuca <ssergioca@gmail.com>
2025-03-15 22:17:43 -0300

    Publisher

--- publisher/Publischer/Program.cs
+++ publisher/Publischer/Program.cs
@@ -1,2 +1,28 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using RabbitMQ.Client;
+using System.Text;
+using System.Threading.Tasks;
+
+static async Task Main(string[] args)
+{
+    var factory = new ConnectionFactory()
+    {
+        HostName = "localhost",
+        UserName = "guest",
+        Password = "guest"
+    };
+
+    using var connection = await factory.CreateConnectionAsync();
+    using var channel = await connection.CreateChannelAsync();
+
+    await channel.QueueDeclareAsync(queue: "filaTESTE", durable: false, exclusive: false, autoDelete: false, arguments: null);
+    const string mensagem = "Primeiro Teste com RABBITMQ";
+
+    var body = Encoding.UTF8.GetBytes(mensagem);
+
+    await channel.BasicPublishAsync(exchange: string.Empty, routingKey: "filaTESTE", body: body);
+    Console.WriteLine("[*] Enviando a mensagem");
+    Console.WriteLine("Pressionone qualquer tecla");
+    Console.ReadLine();
+}
+await Main([]);
